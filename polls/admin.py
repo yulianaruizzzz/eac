1bbc5ade8d412044b8d0c4fd6628732e9bc2cf32
isard <isard@debian>
2025-02-10 18:17:34 +0100

    FINAL

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 
 from .models import Question, Choice
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    inlines = [ChoiceInline]
+    search_fields = ["question_text"]
+    list_filter = ["pub_date"]
+admin.site.register(Question, QuestionAdmin) 
 admin.site.register(Choice)
 # Register your models here.
--- polls/tests_selenium.py
+++ polls/tests_selenium.py
@@ -1,31 +1,25 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium import webdriver
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import NoSuchElementException
-import time
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from django.contrib.auth.models import User
 
 class MySeleniumTests(StaticLiveServerTestCase):
-
+    # no crearem una BD de test en aquesta ocasió (comentem la línia)
+    #fixtures = ['testdb.json',]
+ 
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        options = Options()
-        options.add_argument("--headless")  # Para ejecución en GitHub Actions
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-dev-shm-usage")
-        cls.selenium = webdriver.Chrome(options=options)
+        opts = Options()
+        cls.selenium = WebDriver(options=opts)
         cls.selenium.implicitly_wait(5)
-
+        # creem superusuari
+        user = User.objects.create_user("isard", "isard@isardvdi.com", "pirineus")
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
     @classmethod
     def tearDownClass(cls):
         cls.selenium.quit()
@@ -42,17 +36,18 @@ class MySeleniumTests(StaticLiveServerTestCase):
         password_input.send_keys(Keys.RETURN)
 
         # Esperar que cargue la página principal del admin
-        WebDriverWait(self.selenium, 10).until(
-            EC.presence_of_element_located((By.LINK_TEXT, "Questions"))
-        )
+        self.selenium.implicitly_wait(5)
 
         # Ir a la sección de Questions y crear la primera
-        self.selenium.find_element(By.LINK_TEXT, "Questions").click()
-        self.selenium.find_element(By.LINK_TEXT, "Add Question").click()
-
+       # self.selenium.find_element(By.LINK_TEXT, "Questions").click()
+        #self.selenium.find_element(By.XPATH, "//*[contains(text(),'Add question')]").click()
+        self.selenium.find_element(By.XPATH, "//a[@href='/admin/polls/question/add/']").click() 
         # Crear la primera pregunta con 2 choices
         self.selenium.find_element(By.NAME, "question_text").send_keys("¿Quién ganará la liga este año?")
-        self.selenium.find_element(By.NAME, "pub_date").send_keys("2025-02-03 12:00:00")
+        self.selenium.find_element(By.NAME, "pub_date_0").send_keys("2025-02-03")
+        self.selenium.find_element(By.NAME, "pub_date_1").send_keys("12:00:00")
+
+
 
         # Añadir las choices en el menú inline
         self.selenium.find_element(By.NAME, "choice_set-0-choice_text").send_keys("El Barça porque es el mejor jejej")
@@ -60,22 +55,23 @@ class MySeleniumTests(StaticLiveServerTestCase):
         self.selenium.find_element(By.NAME, "_save").click()  # Guardar la pregunta
 
         # Esperar que se guarde y luego crear la segunda pregunta
-        WebDriverWait(self.selenium, 10).until(
-            EC.presence_of_element_located((By.LINK_TEXT, "Add Question"))
+        self.selenium.find_element(By.XPATH, "//a[@href='/admin/polls/question/add/']").click()                  
+
+ # Crear la primera pregunta con 2 choices
+        self.selenium.find_element(By.NAME, "question_text").send_keys("¿Arroz con leche?")
+        self.selenium.find_element(By.NAME, "pub_date_0").send_keys("2025-02-03")
+        self.selenium.find_element(By.NAME, "pub_date_1").send_keys("12:00:00")
+
+
+
+        # Añadir las choices en el menú inline
+        self.selenium.find_element(By.NAME, "choice_set-0-choice_text").send_keys("si")
+        self.selenium.find_element(By.NAME, "choice_set-1-choice_text").send_keys("no")
+        self.selenium.find_element(By.NAME, "_save").click()  # Guardar la pregunta       
+        # Esperar que se guarde y luego crear la segunda pregunta
+        self.selenium.find_element(By.XPATH, "//a[@href='/admin/polls/choice/']").click()           
+        choices = self.selenium.find_elements(By.XPATH, "//th[@class='field-__str__']/a")
+        self.assertEqual(
+            len(choices), 4, f"holapepsicola"
         )
-        
-        self.selenium.find_element(By.LINK_TEXT, "Add Question").click()
-        self.selenium.find_element(By.NAME, "question_text").send_keys("¿Tortilla con o sin cebolla?")
-        self.selenium.find_element(By.NAME, "pub_date").send_keys("2025-02-03 12:30:00")
-
-        self.selenium.find_element(By.NAME, "choice_set-0-choice_text").send_keys("Sin cebolla, ¡qué ascooo!")
-        self.selenium.find_element(By.NAME, "choice_set-1-choice_text").send_keys("Con cebolla, ¡me encanta!")
-        self.selenium.find_element(By.NAME, "_save").click()
-
-        # Comprobar que existen 4 Choices en el menú "Choices"
-        self.selenium.find_element(By.LINK_TEXT, "Choices").click()
-        choices = self.selenium.find_elements(By.CLASS_NAME, "field-choice_text")
-        assert len(choices) >= 4, "No se encontraron las 4 Choices esperadas"
-
-        print("✔ Se crearon correctamente 2 Questions y 4 Choices con respuestas graciosas")
 
